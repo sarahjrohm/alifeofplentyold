23b21704d5fb1db851db020b10dbf3f39c5d6acb
Sarah Rohm <sarahjrohm@gmail.com>
2011-06-25 18:35:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alifefplenty::Application.config.secret_token = 'fd0bae5dfcbfacbf46606f7b4d87850ea1524125c1f6f1f0fdab73f4aa738088e10d6bacfba38653229e2616b203a23289766798dd2ca060d1b009a3e25381c6'
